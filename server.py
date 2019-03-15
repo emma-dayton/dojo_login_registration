232baf882faa7e2456e9d7b5e19d31042cae291d
Emma Dayton <emma.dayton85@gmail.com>
2019-03-14 17:07:47 -0700

    add header and meta info to success.html, add route for /success to server.py

--- server.py
+++ server.py
@@ -5,10 +5,12 @@ app.secret_key = '5153fe473438c82c17e638dd778b6b5e'
 
 
 @app.route('/')
-def survey():
+def registration():
     return render_template('index.html')
 
-
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 if __name__ == "__main__":
